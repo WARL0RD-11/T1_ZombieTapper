f941904ffc446e146de48e99e6f524e3c12c8106
Nishant Verma <vnishant148@gmail.com>
2024-08-21 17:55:24 -0600

    Player Code  _  Movement

--- Assets/Scripts/Player_Behavior.cs
+++ Assets/Scripts/Player_Behavior.cs
@@ -1,14 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Player_Behavior : MonoBehaviour
-{   
+{
 
     void PlayerMovement()
-    {   
-        
+    {
+        float horizontal = 0;
+        float vertical = 0;
+        float GridSize = 1.0f;
+        bool MovingUp = true;
+        KeyCode KeyPressed = KeyCode.Space;
+        Vector3 NewPos;
+
+        if (Input.GetKeyDown(KeyPressed))
+        {
+            if ((transform.position.y >= -5.0f) && (transform.position.y < 5.0f))
+            { 
+                MovingUp = true;
+                Debug.Log("TRUE");
+            }
+            else if (transform.position.y >= 5.0f)
+            {
+                MovingUp = false;
+                Debug.Log("False");
+            }
+
+            if (MovingUp)
+            {
+                NewPos = transform.position + new Vector3(0, GridSize, 0);
+            }
+            else
+            {
+                NewPos = transform.position - new Vector3(0, GridSize, 0);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                horizontal -= 1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                horizontal += 1;
+            }
+
+            transform.position = NewPos;
+        }
     }
+
     // Start is called before the first frame update
     void Start()
     {
