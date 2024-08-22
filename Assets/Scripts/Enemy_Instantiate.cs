4ee306b4e6c682f5219b8fcfff8e74739f5430a1
Nishant Verma <96386344+WARL0RD-11@users.noreply.github.com>
2024-08-22 12:30:53 -0600

    changed enemy spawn time
    Added a random function to change the enemy spawn time.

--- Assets/Scripts/Enemy_Instantiate.cs
+++ Assets/Scripts/Enemy_Instantiate.cs
@@ -6,27 +6,36 @@ public class Enemy_Instantiate : MonoBehaviour
 {
     [SerializeField] public GameObject enemyPrefab;
     [SerializeField] Transform parent;
-    [SerializeField] float interval = 3f;
     float time = 0;
+    GameManager gameManager;
     void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
+        StartCoroutine(UpdateSpawnTime());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Enemy_Behaviour.isGameOver)
+        if (Enemy_Behaviour.isGameOver)
         { return; }
         time += Time.deltaTime;
-        if(time >= interval)
+        if (time >= gameManager.enemySpawnTime)
         {
-            time = 0f;            
+            time = 0f;
             var spawnPosition = new Vector3(transform.position.x, Mathf.RoundToInt(
                 Random.Range(-2, 2)) * 2, 0); //Generate random lane for enemy
-            GameObject spawnGameObject = Instantiate(enemyPrefab, spawnPosition, 
+            GameObject spawnGameObject = Instantiate(enemyPrefab, spawnPosition,
                Quaternion.identity);
             spawnGameObject.transform.parent = parent; // Parent all clones to a single object
+        }
+    }
+    public IEnumerator UpdateSpawnTime()
+    {
+        while (true)
+        {
+            gameManager.enemySpawnTime = 2 + Mathf.RoundToInt(Random.Range(-2, 1));
+            yield return new WaitForSeconds(gameManager.enemySpawnTime);
         }
     }
 }
