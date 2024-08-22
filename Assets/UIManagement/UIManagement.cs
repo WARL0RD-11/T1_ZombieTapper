fd3786b7fd0af2b3dd959d3c42b33aef965e7705
Justin Li <107675909+Justinyutingli@users.noreply.github.com>
2024-08-21 19:46:07 -0600

    Update

---
+++ Assets/UIManagement/UIManagement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagement : MonoBehaviour
+{
+    [SerializeField] public string StartScene_Name;
+    public void StartGame()
+    {
+
+    }
+
+}
