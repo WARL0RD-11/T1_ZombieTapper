29bb2e6279accb8eb9f7628d02cb7ea98a80ba2f
Justin Li <107675909+Justinyutingli@users.noreply.github.com>
2024-08-21 22:55:20 -0700

    Complete StartScene

--- Assets/UIManagement/UIManagement.cs
+++ Assets/UIManagement/UIManagement.cs
@@ -2,13 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManagement : MonoBehaviour
 {
-    [SerializeField] public string StartScene_Name;
+    [SerializeField] public string GameScene_Name;
+    [SerializeField] public GameObject StartMenu;
+    [SerializeField] public GameObject OptionMenu;
+    [SerializeField] public GameObject InstructMenu;
+    [SerializeField] public Slider volumeSlider;
+
+
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        } else {
+            Load();
+        }
+    }
+    //Scene Controller
     public void StartGame()
     {
+        SceneManager.LoadScene(GameScene_Name);
+    }
 
+    public void GetScene(string Scene_Name)
+    {
+        SceneManager.LoadScene(Scene_Name);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    //Panel Controller
+    public void Open_OptionMenu()
+    {
+        OptionMenu.SetActive(true);
+        StartMenu.SetActive(false);
+    }
+    public void Backto_StartMenu()
+    {
+        OptionMenu.SetActive(false);
+        StartMenu.SetActive(true);
+    }
+    public void Open_InstructMenu()
+    {
+        OptionMenu.SetActive(false);
+        InstructMenu.SetActive(true);
+    }
+    public void Backto_OptionMenu()
+    {
+        OptionMenu.SetActive(true);
+        InstructMenu.SetActive(false);
+    }
+
+    //Volume Controller
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
 
 }
