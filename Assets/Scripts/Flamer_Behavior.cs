d136ee699fea4782ed81335ef883f07d99a2435a
Carter Block <carterpblock@gmail.com>
2024-09-03 11:03:31 -0600

    Visual updates + Logic updates

--- Assets/Scripts/Flamer_Behavior.cs
+++ Assets/Scripts/Flamer_Behavior.cs
@@ -7,15 +7,21 @@ public class Flamer_Behavior : MonoBehaviour
 
     private int damage;
 
+    //Every zombie that enters the trigger box gets hit by the flamethrower and gets burnt to death
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        //Still check that it's truly a zombie is overlapping with the trigger
+        //Better safe than sorry
         if (collision.GetComponent<Enemy_Behaviour>() != null)
         {
-            //Destroy(collision.gameObject);
+            //Do damage
             collision.gameObject.GetComponent<Enemy_Behaviour>().ReduceHealth(damage);
         }
     }
 
+    //Set the damage that the flamer does
+    //Called in Soldier_Behavior.cs Start() function
+    //In the S_B serialize fields
     public void SetFlamerDamage(int amount)
     {
         damage = amount;
