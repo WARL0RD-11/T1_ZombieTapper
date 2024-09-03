1d0dbfc27cb850085a228685dfa841f7f78bd046
Nishant Verma <96386344+WARL0RD-11@users.noreply.github.com>
2024-09-03 10:02:04 -0600

    refactoring enemy code

--- Assets/Scripts/Enemy_Behaviour.cs
+++ Assets/Scripts/Enemy_Behaviour.cs
@@ -6,38 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class Enemy_Behaviour : MonoBehaviour
 {
-    [SerializeField] float enemySpeed = 0.25f;
     [SerializeField] int health = 1;
-    [SerializeField] float attackCoolDown = 1f;
     [SerializeField] int enemyPower = 1;
+    [SerializeField] float enemySpeed = 0.25f;
+    [SerializeField] float attackCoolDown = 1f;
     [SerializeField] GameObject bloodPoolPrefab;
 
     GameManager gameManager;
     Animator animator;
     GameObject sandBagObject;
-    Obstacle_Behaviour obstacleObject;
+    AudioManager audioManager;
+
     bool canCharacterMove = true;
     bool canAttack = false;
-    private List<GameObject> targetsInRange = new List<GameObject>();
-
-    //Audio
-    AudioManager audioManager;
+    List<GameObject> targetsInRange = new List<GameObject>();
 
     private void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("audio").GetComponent<AudioManager>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         gameManager = FindAnyObjectByType<GameManager>();
         animator = GetComponent<Animator>();
         sandBagObject = GameObject.FindGameObjectWithTag("Finish");
-        //obstacleObject = FindAnyObjectByType<Obstacle_Behaviour>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (!gameManager.GetGameStatus() && canCharacterMove)
@@ -47,11 +42,9 @@ public class Enemy_Behaviour : MonoBehaviour
         else if(gameManager.GetGameStatus()) 
         {
             canCharacterMove = false;
-            //canAttack = false;
             animator.SetBool("isZombieIdle", true);
             animator.SetBool("isAttacking", false);
         }
-        //Check if enemy reached the barricade
         if (transform.position.x >= sandBagObject.transform.position.x)
         {
             animator.SetBool("isZombieEnd", true);
@@ -65,21 +58,13 @@ public class Enemy_Behaviour : MonoBehaviour
     }
     private void EnemyMovement()
     {
-        //Move enemy on the x-axis
         transform.position += new Vector3(1.0f, 0f, 0) * enemySpeed * Time.deltaTime;
     }
-
-    //Function to be called by soldiers when they are delivered the correct item
     public void OnDeath()
     {
-        //gameManager.AddScore(1);
-        //Set the speed to 0 so corpses can't move
         enemySpeed = 0.0f;
         animator.SetBool("isZombieDead", true);
         GetComponent<BoxCollider2D>().enabled = false;
-
-        //audioManager.PlaySFX(audioManager.ZombieEatFinal_Audio);
-
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -88,7 +73,6 @@ public class Enemy_Behaviour : MonoBehaviour
             targetsInRange.Add(collision.gameObject);
             canCharacterMove = false;
             canAttack = true;
-            //animator.SetBool("isZombieIdle", true);
             animator.SetBool("isAttacking", true);
         }
     }
@@ -98,7 +82,6 @@ public class Enemy_Behaviour : MonoBehaviour
         {
             canCharacterMove = true;
             canAttack = false;
-            //animator.SetBool("isZombieIdle", false);
             animator.SetBool("isAttacking", false);
         }
     }
@@ -123,11 +106,8 @@ public class Enemy_Behaviour : MonoBehaviour
     {
         Vector3 deathPos = transform.position;
         deathPos.y = deathPos.y - 0.4f;
-
         Instantiate(bloodPoolPrefab, deathPos, Quaternion.identity);
-
         audioManager.PlaySFX(audioManager.ZombieDead_Audio);
-
         Destroy(this.gameObject);
     }
 
